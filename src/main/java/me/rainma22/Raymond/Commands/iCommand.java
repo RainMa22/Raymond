4d553038bf44cd0e9b854590a0e65d5ee9c5a038
Rainma22 <d2rain@126.com>
2025-02-15 16:55:08 -0800

    Easter Egg: use reply instead

--- src/main/java/me/rainma22/Raymond/Commands/iCommand.java
+++ src/main/java/me/rainma22/Raymond/Commands/iCommand.java
@@ -8,7 +8,7 @@ public interface iCommand extends BiConsumer<MessageReceivedEvent, String[]> {
     @Override
     default void accept(MessageReceivedEvent event, String[] cmds) {
         if (cmds[0].toLowerCase().contains("everything")) {
-            event.getChannel().sendMessage("https://tenor.com/view/" +
+            event.getMessage().reply("https://tenor.com/view/" +
                     "everything-everywhere-all-at-once-daniel-kwan-daniel-scheinert-quote-life-gif-17047606721800614267").queue();
         } else {
             event.getChannel().sendMessage("No Such Command: " + cmds[0]).queue();
