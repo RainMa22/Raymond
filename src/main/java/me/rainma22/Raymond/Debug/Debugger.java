74743a6eb09dc20ab5f655539e304be0f3ceb8c4
RainMa22 <ruiruima22@gmail.com>
2026-05-27 14:26:48 -0700

    add debug level

--- src/main/java/me/rainma22/Raymond/Debug/Debugger.java
+++ src/main/java/me/rainma22/Raymond/Debug/Debugger.java
@@ -8,6 +8,12 @@ import java.io.PrintStream;
 public class Debugger {
 
     private static Debugger instance = null;
+    public static final int VERY_VERBOSE = 2;
+    public static final int VERBOSE = 1;
+    public static final int LOG = 0;
+    public static final int WARNING = -1;
+    public static final int ERROR = -2;
+    private int logLevel = 0;
     private PrintStream out = System.out;
 
     private Debugger() {
@@ -17,14 +23,24 @@ public class Debugger {
         if (instance == null) {
             instance = new Debugger();
         }
-       return instance;
+        return instance;
     }
 
     public void setOut(PrintStream out) {
         this.out = out;
     }
 
+    public void setLogLevel(int newLevel) {
+        logLevel = newLevel;
+    }
+
     public void log(Object x) {
-        this.out.println(x);
+        log(x, LOG);
+    }
+
+    public void log(Object x, int logLevel) {
+        if (logLevel <= this.logLevel) {
+            this.out.println(x);
+        }
     }
 }
